f33a4393fe740e14fdf7dca37c10fb8a996ecf04
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-01-12 11:13:44 -0500

    ...

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -1,25 +1,35 @@
 package asciimirror;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String input = userInput(scanner);
-        System.out.printf("%s%n", input);
-        System.out.println("            ^__^");
-        System.out.println("    _______/(oo)");
-        System.out.println("/\\/(       /(__)");
-        System.out.println("   | w----||    ");
-        System.out.println("   ||     ||    ");
+        String input = userInput();
+        File file = new File(input);
+
+        try {
+            Scanner read = new Scanner(file);
+            while(read.hasNext()){
+                System.out.println(read.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
 
     }
 
-    public static String userInput(Scanner scanner) {
+    public static String userInput() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Input the file path:");
         String input = scanner.nextLine();
 
         return input;
+    }
+    public static void printAnimal(){
+
     }
 }
