9ac1f3948863c00ed5c2cff78d9bf027ec2f0c22
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-01-13 12:07:37 -0500

    ...

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -1,23 +1,32 @@
 package asciimirror;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
 
         String input = userInput();
-        File file = new File(input);
 
         try {
-            Scanner read = new Scanner(file);
-            while(read.hasNext()){
-                System.out.println(read.nextLine());
-            }
-        } catch (FileNotFoundException e) {
+            List<String> list = new ArrayList<>();
+
+            list =Files.readAllLines(Paths.get(input));
+            int max = maxLenght(list);
+
+            List<String> lista = reverseInit(list,max);
+
+
+            print(lista);
+
+        } catch (IOException e) {
             System.out.println("File not found!");
+        }catch (InvalidPathException e ){
+            System.out.println("File not found");
         }
 
     }
@@ -26,10 +35,47 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input the file path:");
         String input = scanner.nextLine();
-
         return input;
     }
-    public static void printAnimal(){
-
+    public static int maxLenght(List<String> a) {
+        int max = 0;
+        for (String x: a){
+            max = Math.max(x.length(),max);
+        }
+        return max;
+    }
+    public static void print(List<String> lista){
+        for (String x : lista){
+            System.out.println(x);
+        }
+    }
+    public static List<String> reverseInit(List<String> list, int max){
+        List<String> result_str= new ArrayList<>();
+        for (String s:list) {
+            StringBuilder sb=new StringBuilder();
+            sb.append(s);
+            sb.append(" ".repeat(max-s.length()));
+            StringBuilder res = new StringBuilder();
+            res.append(sb);
+            res.append(" | ");
+            sb.reverse();
+            for (int i=0;i<sb.length();i++) {
+                switch(sb.charAt(i)){
+                    case '\\': res.append("/");break;
+                    case '/': res.append("\\");break;
+                    case '}': res.append("{");break;
+                    case '{': res.append("}");break;
+                    case ']': res.append("[");break;
+                    case '[': res.append("]");break;
+                    case ')': res.append("(");break;
+                    case '(': res.append(")");break;
+                    case '<': res.append(">");break;
+                    case '>': res.append("<");break;
+                    default: res.append(sb.charAt(i));break;
+                }
+            }
+            result_str.add(res.toString());
+        }
+        return result_str;
     }
 }
--- ASCII Mirror/task/test/MirrorTest.java
+++ ASCII Mirror/task/test/MirrorTest.java
@@ -4,12 +4,16 @@ import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testing.TestedProgram;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MirrorTest extends StageTest {
+
   Object[][] test_data = {
           {"./test/example1.txt",1},
           {"./test/example2.txt",1},
@@ -63,11 +67,42 @@ public class MirrorTest extends StageTest {
         throw new WrongAnswer("When the user inputs a file, that can be correctly opened - output should " +
                 "contain as much lines, as there were in file.");
       }
-
+      int max=0;
+      for (String s:file_str) {
+        if(s.length()>=max){
+          max=s.length();
+        }
+      }
+      List<String> result_str= new ArrayList<>();
+      for (String s:file_str) {
+        StringBuilder sb=new StringBuilder();
+        sb.append(s);
+        sb.append(" ".repeat(max-s.length()));
+        StringBuilder res = new StringBuilder();
+        res.append(sb);
+        res.append(" | ");
+        sb.reverse();
+        for (int i=0;i<sb.length();i++) {
+          switch(sb.charAt(i)){
+            case '\\': res.append("/");break;
+            case '/': res.append("\\");break;
+            case '}': res.append("{");break;
+            case '{': res.append("}");break;
+            case ']': res.append("[");break;
+            case '[': res.append("]");break;
+            case ')': res.append("(");break;
+            case '(': res.append(")");break;
+            case '<': res.append(">");break;
+            case '>': res.append("<");break;
+            default: res.append(sb.charAt(i));break;
+          }
+        }
+        result_str.add(res.toString());
+      }
       for (int i=0;i<list.size();i++) {
-        if (!list.get(i).equals(file_str.get(i))) {
-          throw new WrongAnswer("When the user inputs a file, that can be correctly opened - your program " +
-                  "should output all the text from that file line by line");
+        if (list.get(i).length()!=result_str.get(i).length() || !list.get(i).equals(result_str.get(i))) {
+          throw new WrongAnswer("When the user inputs a file, that can be correctly opened - " +
+                  "each line in output should match the following pattern: \"{modified line} | {reversed modified line}\"");
         }
       }
     }
